1ee282d4b3eda04fa9e67c909203cd1bbb5a06ef
Anton <anton.uranov@yandex.ru>
2024-04-15 14:34:13 +0700

    added Validation - Admin Basket

--- apps/baskets/admin.py
+++ apps/baskets/admin.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from apps.baskets.models import Basket, BasketRow
 
-# from django.core.exceptions import ValidationError
-# from django.forms import BaseInlineFormSet
 
-
-# class BasketRowInLineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             if form.cleaned_data["DELETE"]:
-#                 continue
-#             if form.cleaned_data["qty"] > form.cleaned_data["item"].count:
-#                 raise ValidationError(
-#                     f"Max Limit count  [{form.cleaned_data['item'].product.name}] - {form.cleaned_data['item'].count}"
-#                 )
-#         super().clean()
+class BasketRowInLineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_all = 0
+        for form in self.forms:
+            if form.cleaned_data["DELETE"]:
+                continue
+            if form.cleaned_data["qty"] > form.cleaned_data["item"].count:
+                raise ValidationError(
+                    f"Превышено кол-во  [{form.cleaned_data['item'].catalog.name}] - {form.cleaned_data['item'].count}"
+                )
+            else:
+                count_all += form.cleaned_data["qty"]
+                if count_all > form.cleaned_data["item"].count:
+                    raise ValidationError(
+                        f"Превыш. лимит  [{form.cleaned_data['item'].catalog.name}] - {form.cleaned_data['item'].count}"
+                    )
+        super().clean()
 
 
 class BasketRowInLine(admin.TabularInline):
     model = BasketRow
     extra = 0
     readonly_fields = ["price_unit", "summa_price", "is_active_item"]
-    # formset = BasketRowInLineFormset
+    formset = BasketRowInLineFormset
 
     @admin.display(boolean=True)
     def is_active_item(self, obj):
