ae03a68c2934d934bc23fd93a94e6ddad9e80b95
Anton <anton.uranov@yandex.ru>
2024-04-22 10:36:07 +0700

    added Stop-world (Api)

--- apps/api/serializers/items.py
+++ apps/api/serializers/items.py
@@ -1,3 +1,5 @@
+import re
+
 from dynamic_rest.fields import DynamicMethodField, DynamicRelationField
 from taggit.serializers import TaggitSerializer, TagListSerializerField
 
@@ -5,6 +7,7 @@ from apps.api.serializers.abstract_dynamic import BaseModelSerializer
 from apps.api.serializers.holder import ImageHolderSerializer, VideoHolderSerializer
 from apps.products.models import Catalog, Item, Product, Review
 from apps.users.models import User
+from apps.utilities.models import BlacklistedWord
 
 
 class ProductItemSerializer(BaseModelSerializer):
@@ -63,10 +66,10 @@ class ReviewSerializer(BaseModelSerializer):
     images = DynamicRelationField(ImageHolderSerializer, many=True)
     videos = DynamicRelationField(VideoHolderSerializer, many=True)
 
-    # def validate_text(self, value: str) -> str:
-    #     blacklisted_words = BlacklistedWord.objects.values_list("word", flat=True)
-    #     if not blacklisted_words:
-    #         return value
-    #     blacklisted_words = "|".join(blacklisted_words)
-    #     pattern = re.compile(blacklisted_words, re.IGNORECASE)
-    #     return pattern.sub("***", value)
+    def validate_text(self, value: str) -> str:
+        blacklisted_words = BlacklistedWord.objects.values_list("word", flat=True)
+        if not blacklisted_words:
+            return value
+        blacklisted_words = "|".join(blacklisted_words)
+        pattern = re.compile(blacklisted_words, re.IGNORECASE)
+        return pattern.sub("****", value)
