d7accea39210199d0b917ae5281e0472fe42769d
Anton <anton.uranov@yandex.ru>
2024-04-22 11:10:03 +0700

    added payment-link

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from apps.api.views.baskets import BasketRowViewSet
 from apps.api.views.items import ItemViewSet, ReviewViewSet
 from apps.api.views.load_data import import_file
-from apps.api.views.orders import OrderViewSet
+from apps.api.views.orders import OrderViewSet, fake_leadpay_link
 
 router = DefaultRouter()
 router.register("items", ItemViewSet)
@@ -18,4 +18,5 @@ urlpatterns = [
     path("", include("djoser.urls")),
     path("products-import-file/", import_file),
     path("", include(router.urls)),
+    path("fake-leadpay-link/", fake_leadpay_link),
 ]
--- apps/api/views/orders.py
+++ apps/api/views/orders.py
@@ -1,13 +1,14 @@
 from django.db import transaction
 from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.exceptions import APIException, ValidationError
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from apps.api.serializers.orders import OrderSerializer
 from apps.api.views.abstract_dynamic import BaseModelViewSet
 from apps.orders.models import Order
+from apps.orders.service_pay import LeadpayError, generate_leadpay_payment_link
 
 
 class OrderViewSet(BaseModelViewSet):
@@ -62,3 +63,24 @@ class OrderViewSet(BaseModelViewSet):
         serializer = self.get_serializer(order)
         # serializer = OrderSerializer(order)  # Выводим через сериализатор итог покупок
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(methods=["get"], detail=True, url_path="payment-link")
+    def payment_link(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        try:
+            print("отправил объект в функцию")
+            link = generate_leadpay_payment_link(obj)
+        except LeadpayError as ex:
+            raise APIException(detail=str(ex), code=status.HTTP_424_FAILED_DEPENDENCY)
+
+        return Response({"link for pay: ": link}, status=status.HTTP_200_OK)
+
+
+# Создали Фейковый адрес (якобы Шлюза для отправки Post-запроса на получения ссылки для оплаты)
+@api_view(http_method_names=["post"])
+@permission_classes([AllowAny])
+def fake_leadpay_link(request, *args, **kwargs):
+    data = request.data
+    link_result_pay = {"url": f'http://{data["login"]}.ru/id-{data["id"]}/{data["hash"]}'}
+    return Response(link_result_pay, status=status.HTTP_200_OK)
